6d440d681075ffb073fd9f50989379312483fe06
Dschnepf7 <domschnepf1@gmail.com>
2023-03-20 19:54:57 -0500

    added thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,28 @@
+const { Schema, model } = require('mongoose');
 
-    
+const textValidator = (text) => {
+  if (text && (text.length < 1 || text.length > 280)) {
+    return false;
+  }
+  return true;
+};
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    validate: [textValidator, 'Text must be between 1 and 280 characters'],
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
