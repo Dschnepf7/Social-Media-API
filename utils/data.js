e9ee037444609b455d08ebaa5ba4ec6929903894
Dschnepf7 <domschnepf1@gmail.com>
2023-03-21 18:39:48 -0500

    added data.js

---
+++ utils/data.js
@@ -0,0 +1,13 @@
+const user1 = {
+    username: "JohnDoe",
+    email:"JohnDoe@example.com"
+}
+
+const thought1 = {
+    thoughtText: "This is the thoughtText",
+    username: "JohnDoe"
+}
+
+module.exports = {
+    user1, thought1
+}
